508bf424306372d7f9e36767a6313aff1febfadd
大胃王 <253498229@qq.com>
2018-12-09 13:27:48 +0800

    add GlobalMethodSecurity

--- src/main/java/com/example/client/controller/TestController.java
+++ src/main/java/com/example/client/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.example.client.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,6 +27,22 @@ public class TestController {
     public ResponseEntity privateMethod() {
         Map<String, Object> map = new HashMap<>(1);
         map.put("data", "privateMethod");
+        return ok(map);
+    }
+
+    @PreAuthorize("hasAnyRole('USER')")
+    @GetMapping("/user")
+    public ResponseEntity userMethod() {
+        Map<String, Object> map = new HashMap<>(1);
+        map.put("data", "userMethod");
+        return ok(map);
+    }
+
+    @PreAuthorize("hasAnyRole('VIP')")
+    @GetMapping("/vip")
+    public ResponseEntity vipMethod() {
+        Map<String, Object> map = new HashMap<>(1);
+        map.put("data", "vipMethod");
         return ok(map);
     }
 }
--- src/test/java/com/example/client/controller/TestControllerTest.java
+++ src/test/java/com/example/client/controller/TestControllerTest.java
@@ -1,10 +1,11 @@
 package com.example.client.controller;
 
+import com.example.client.config.ResourceConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -14,8 +15,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
-@AutoConfigureMockMvc
+@WebMvcTest(TestController.class)
+@Import(ResourceConfig.class)
 public class TestControllerTest {
 
     @Autowired
@@ -31,7 +32,7 @@ public class TestControllerTest {
     }
 
     @Test
-    public void privateMethod_401() throws Exception {
+    public void privateMethod_isUnauthorized() throws Exception {
         mockMvc.perform(get("/private"))
                 .andExpect(status().isUnauthorized())
         ;
@@ -46,7 +47,50 @@ public class TestControllerTest {
         ;
     }
 
+    @Test
+    @WithMockUser(roles = "USER")
+    public void testRole_user() throws Exception {
+        mockMvc.perform(get("/user"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").isNotEmpty())
+                .andExpect(jsonPath("$.data").isNotEmpty())
+        ;
 
+        mockMvc.perform(get("/vip"))
+                .andExpect(status().isForbidden())
+        ;
+    }
+
+
+    @Test
+    @WithMockUser(roles = "VIP")
+    public void testRole_vip() throws Exception {
+        mockMvc.perform(get("/vip"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").isNotEmpty())
+                .andExpect(jsonPath("$.data").isNotEmpty())
+        ;
+
+        mockMvc.perform(get("/user"))
+                .andExpect(status().isForbidden())
+        ;
+    }
+
+    @Test
+    @WithMockUser(roles = {"USER", "VIP"})
+    public void testRole_admin() throws Exception {
+        mockMvc.perform(get("/user"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").isNotEmpty())
+                .andExpect(jsonPath("$.data").isNotEmpty())
+        ;
+
+        mockMvc.perform(get("/vip"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").isNotEmpty())
+                .andExpect(jsonPath("$.data").isNotEmpty())
+        ;
+    }
 
 
 }
